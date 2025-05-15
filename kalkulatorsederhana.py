e55a156cfa7244338a3c4ba1fc73bbae3c5e1184
Aabamz <zabranfarzana@gmail.com>
2025-05-15 16:06:35 +0700

    Add files via upload
    study kasus

---
+++ kalkulatorsederhana.py
@@ -0,0 +1,17 @@
+a_zabran = 10
+b_zabran = 5
+op_zabran = '/' 
+hasil_zabran = 0
+
+if op_zabran == '+':
+    hasil_zabran = a_zabran + b_zabran
+elif op_zabran == '-':
+    hasil_zabran = a_zabran - b_zabran
+elif op_zabran == '*':
+    hasil_zabran = a_zabran * b_zabran
+elif op_zabran == '/':
+    hasil_zabran = a_zabran / b_zabran
+elif op_zabran == '//':
+    hasil_zabran = a_zabran // b_zabran
+
+print("Hasil=",hasil_zabran)
